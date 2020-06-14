86b966f48644bba5084dc6ef4e189e5d09e4cef1
Abdullah Khan <contact@khanabdullah.com>
2020-06-14 16:23:08 -0600

    restructure scenarios object array
    Co-authored-by: Shahab <shahab.salem6@gmail.com>
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

---
+++ src/scenarios.js
@@ -0,0 +1,54 @@
+const scenarios = [
+	{
+		imageURL: require("./images/phish/01.png"),
+		answer: false,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/legit/01.png"),
+		answer: true,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/phish/02.png"),
+		answer: false,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/legit/02.png"),
+		answer: true,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/phish/03.png"),
+		answer: false,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/legit/03.png"),
+		answer: true,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/phish/04.png"),
+		answer: false,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/legit/04.png"),
+		answer: true,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/phish/05.png"),
+		answer: false,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+	{
+		imageURL: require("./images/legit/05.png"),
+		answer: true,
+		tips: ["tip 1", "tip 2", "tip 3"],
+	},
+];
+
+export default scenarios;
