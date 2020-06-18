6c96c036f637ea7420cfea2014a62d45ca72697f
Abdullah Khan <contact@khanabdullah.com>
2020-06-18 17:54:49 -0600

    improve responsiveness on homepage and game page
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -7,16 +7,18 @@ function Homepage() {
 		<div className="App fade-in">
 			<div className="accent-bar bg-both" />
 			<div className="page homepage-section">
-				<div className="page-column">
-					<h1 className="page-heading">Phish or Legit</h1>
-					<p className="subheading">
-						Test your skills by correctly classifying each <br />
-						scenario as phish or legit.
-					</p>
-					<p className="button-description">Feeling Confident?</p>
-					<Link to="/play" className="button button-light button-large">
-						Play
-					</Link>
+				<div className="page-column flex-align-center flex-column">
+					<div>
+						<h1 className="page-heading">Phish or Legit</h1>
+						<p className="subheading">
+							Test your skills by correctly classifying each <br />
+							scenario as phish or legit.
+						</p>
+						<p className="button-description">Feeling Confident?</p>
+						<Link to="/play" className="button button-light button-large">
+							Play
+						</Link>
+					</div>
 				</div>
 				<div className="page-column flex-center">
 					<div className="card how-to-play">
