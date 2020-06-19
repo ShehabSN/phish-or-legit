cd96d70496b6184aeab2a13315c8a65ba1f506f5
Abdullah Khan <contact@khanabdullah.com>
2020-06-18 19:20:12 -0600

    fix props.location bug on game over page
    Co-authored-by: Shahab <shahab.salem6@gmail.com>
    Signed-off-by: Abdullah Khan <contact@khanabdullah.com>

--- src/components/Gameover.js
+++ src/components/Gameover.js
@@ -1,25 +1,33 @@
 import React from "react";
-import { withRouter } from "react-router";
+import { withRouter, Redirect } from "react-router";
 
 const Gameover = props => {
 	return (
 		<div className="App">
 			<div className="game-over fade-in">
-				<h1>Game Over</h1>
-				<h2
-					className={
-						props.location.state.correctAmount < 5 ? "text-red" : "text-green"
-					}>
-					You got {props.location.state.correctAmount}/10 correct!
-				</h2>
-				<div
-					style={{ paddingLeft: 30, paddingRight: 30, marginTop: 40 }}
-					onClick={() => {
-						props.history.push("/play");
-					}}
-					className="button button-light">
-					Play Again
-				</div>
+				{!props.location.state ? (
+					<Redirect to="/" />
+				) : (
+					<>
+						<h1>Game Over</h1>
+						<h2
+							className={
+								props.location.state.correctAmount < 5
+									? "text-red"
+									: "text-green"
+							}>
+							You got {props.location.state.correctAmount}/10 correct!
+						</h2>
+						<div
+							style={{ paddingLeft: 30, paddingRight: 30, marginTop: 40 }}
+							onClick={() => {
+								props.history.push("/play");
+							}}
+							className="button button-light">
+							Play Again
+						</div>
+					</>
+				)}
 			</div>
 			<div
 				style={{
